5249f06dfff09d916333aeec08383707c66a1396
Vanessa Ating <atingenkay@gmail.com>
2019-04-24 22:02:37 +0100

    fix config file

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://nkoyo@localhost/friends_list"
+SQLALCHEMY_DATABASE_URI = 'postgresql://nkoyo@localhost/friends_list'
